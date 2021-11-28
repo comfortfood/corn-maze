bf4a4909a5a685844e0739b39053dced2510acbb
Wyatt Tall <wotall@gmail.com>
2021-11-28 13:22:16 -0600

    replit fix

--- main.py
+++ main.py
@@ -946,7 +946,8 @@ def print_screen(screen):
     return display
 
 
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0'
-    )
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
